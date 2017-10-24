0acf9c4aab63a6c8ec995d97871768756117e84e
StevenBorst <sborst22@gmail.com>
2017-10-24 13:53:50 -0400

    Started views

--- project2/webscale/synthesizer/views.py
+++ project2/webscale/synthesizer/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import *
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    return render(
+        request,
+        'index.html',
+        
+    )
