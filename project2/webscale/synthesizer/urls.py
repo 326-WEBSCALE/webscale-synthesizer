2ec438f70a22fc4bc8dd015155ed2afc243cebf0
StevenBorst <sborst22@gmail.com>
2017-10-26 01:39:12 -0400

    Map urls to their html pages

--- project2/webscale/synthesizer/urls.py
+++ project2/webscale/synthesizer/urls.py
@@ -5,4 +5,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^faq/$', views.faq, name='faq'),
+    url(r'^feed/$', views.feed, name='feed'),
+    url(r'^discussion/$', views.discussion, name='discussion'),
+    url(r'^profile/$', views.profile, name='profile'),
+
 ]
--- project2/webscale/synthesizer/views.py
+++ project2/webscale/synthesizer/views.py
@@ -11,5 +11,45 @@ def index(request):
     return render(
         request,
         'index.html',
-        
+    )
+
+def about(request):
+    """
+    View function for the about page.
+    """
+    return render(
+        request,
+        'synthesizer/about.html',
+    )
+def discussion(request):
+    """
+    View function for the discussion page.
+    """
+    return render(
+        request,
+        'synthesizer/discussion.html',
+    )
+def faq(request):
+    """
+    View function for the faq page.
+    """
+    return render(
+        request,
+        'synthesizer/faq.html',
+    )
+def feed(request):
+    """
+    View function for the feed page.
+    """
+    return render(
+        request,
+        'synthesizer/feed.html',
+    )
+def profile(request):
+    """
+    View function for the profile page.
+    """
+    return render(
+        request,
+        'synthesizer/profile.html',
     )
