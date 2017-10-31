8ee439070d939ed43225995451b5644dc0adf7c7
Jared Holzman <jared@holzman.io>
2017-10-31 17:11:34 -0400

    Fetch all user snippets in view controller.

--- project2/webscale/synthesizer/views.py
+++ project2/webscale/synthesizer/views.py
@@ -10,17 +10,19 @@ def index(request):
     # Generate counts of some of the main objects
     d = '14debd2def58452ab795a6b973977920'
     snippet = Snippit.objects.get(pk=d)
-    print("THING: {0}".format(snippet.get_description()))
+    all_user_snippets = map(lambda snip: (snip.get_id(), snip.get_name(), snip.get_description()),
+                            Snippit.objects.filter(user_id=snippet.get_user_id()))
     return render(
         request,
         'index.html',
         context={'page_title': 'W E B S C A L E',
-                 'snippet_name': snippet.get_name(),
+                 'snippet_name': ": {0}".format(snippet.get_name()),
                  'snippet_user_id': snippet.get_user_id(),
                  'snippet_description': snippet.get_description(),
                  'snippet_text': snippet.get_program_text(),
                  'snippet_result': snippet.get_synthesizer_result(),
                  'snippet_is_public': snippet.get_is_public(),
+                 'all_user_snippets': all_user_snippets,
         },
     )
 
