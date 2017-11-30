f98c7ff29b4171cd0e64d874b27569fe08551387
StevenBorst <sborst22@gmail.com>
2017-11-30 13:14:58 -0500

    Create Final Submission folder

---
+++ Final Submission/webscale/synthesizer/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^program/(\w*)$', views.index, name='program'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^faq/$', views.faq, name='faq'),
+    url(r'^feed/$', views.feed, name='feed'),
+    url(r'^discussion/(\w*)$', views.discussion, name='discussion'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^profile/edit$', views.profile_edit, name='profile_edit'),
+    url(r'^profile/(\w*)$', views.profile, name='profile'),
+]
