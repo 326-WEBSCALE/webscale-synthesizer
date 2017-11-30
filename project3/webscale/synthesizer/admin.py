b70dd5b644a5508d0258e919b995a6a49e070480
StevenBorst <sborst22@gmail.com>
2017-11-30 13:41:54 -0500

    Delete folder-style version control

--- project3/webscale/synthesizer/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ApplicationTable,Snippit,SnippitData,HolesTable,GoogleAuth,Comment
-# Register your models here.
-admin.site.register(ApplicationTable)
-admin.site.register(Snippit)
-admin.site.register(SnippitData)
-admin.site.register(HolesTable)
-admin.site.register(GoogleAuth)
-admin.site.register(Comment)
--- project3/webscale/synthesizer/templates/README.md
+++
@@ -1,2 +0,0 @@
-# Webscale Synthesizer Project 1
-This a mock ui for the W E B S C A L E team's Synthesizer project
